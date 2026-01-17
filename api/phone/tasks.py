f088de713b960eacbb053e56e127b230f2ca6988
Marc <mtakoumba@gmail.com>
2026-01-17 12:13:30 +0100

    correction du lead

--- api/phone/tasks.py
+++ api/phone/tasks.py
@@ -1,7 +1,12 @@
 from celery import shared_task
 from .sender import PhoneSender
 
-@shared_task(bind=True, autoretry_for=(Exception,), retry_kwargs={"max_retries": 3})
+@shared_task(
+    bind=True,
+    queue="scheduler",
+    autoretry_for=(Exception,),
+    retry_kwargs={"max_retries": 3},
+)
 def trigger_call_task(self, phone_number):
     sender = PhoneSender()
     return sender.click_to_call(phone_number)
