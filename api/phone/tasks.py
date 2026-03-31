e7d94e3e61abe66c4c632bb534eacd9dc4524e61
Marc <mtakoumba@gmail.com>
2026-03-31 18:30:26 +0200

    V5

--- api/phone/tasks.py
+++ api/phone/tasks.py
@@ -1,12 +1,5 @@
-from celery import shared_task
 from .sender import PhoneSender
 
-@shared_task(
-    bind=True,
-    queue="scheduler",
-    autoretry_for=(Exception,),
-    retry_kwargs={"max_retries": 3},
-)
 def trigger_call_task(self, phone_number):
     sender = PhoneSender()
     return sender.click_to_call(phone_number)
